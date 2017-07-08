868d0b1b9d5b9179a094fcb5e06dfedb761a4a65
Fabio Takaki <fabio@takaki.me>
2017-07-07 23:15:28 -0300

    added chat, conversation and messages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-
+  get 'chat/index'
   root to: 'home#index'
+  resources :conversations, only: [:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
